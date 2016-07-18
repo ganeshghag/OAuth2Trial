08f9ad23ace6735d8e49f09c7079dd896ebd56d6
Ganesh M. Ghag <ganeshmg@mastek.com>
2016-07-18 14:51:55 +0530

    first commit

---
+++ src/test/java/com/ghag/rnd/security/OAuth2TrialApplicationTests.java
@@ -0,0 +1,18 @@
+package com.ghag.rnd.security;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = OAuth2TrialApplication.class)
+@WebAppConfiguration
+public class OAuth2TrialApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
